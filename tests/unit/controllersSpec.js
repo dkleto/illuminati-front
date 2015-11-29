f3dd2ec599bc85ea5a771fa699552b35d87fee28
Adam Riddell <riddelladam42@gmail.com>
2015-11-29 23:27:59 +1100

    Add skeleton for schedule spec

---
+++ tests/unit/controllersSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('Illuminati controllers', function() {
+
+  describe('scheduleCtrl', function(){
+    var scope, ctrl, $httpBackend;
+
+    beforeEach(module('illuminati'));
+  });
+});
