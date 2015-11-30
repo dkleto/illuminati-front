f4877fd73593065b19aed619dfd3124340c3044e
Adam Riddell <riddelladam42@gmail.com>
2015-11-30 21:59:28 +1100

    Inject dependencies for unit tests

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -6,5 +6,10 @@ describe('Illuminati controllers', function() {
     var scope, ctrl, $httpBackend;
 
     beforeEach(module('illuminati'));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      scope = $rootScope.$new();
+      $httpBackend = _$httpBackend_;
+      ctrl = $controller('scheduleCtrl', {$scope: scope});
+    }));
   });
 });
