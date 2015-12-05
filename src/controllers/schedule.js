5b991290e7b3fd641684feae3f900c8564e23c3b
Adam Riddell <riddelladam42@gmail.com>
2015-12-05 16:52:44 +1100

    Add env config dependency to schedule controller

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var illuminati = angular.module('illuminati', []);
+var illuminati = angular.module('illuminati', ['illuminati-conf']);
 
-illuminati.controller('scheduleCtrl', ['$scope', '$http', function($scope, $http) {
+illuminati.controller('scheduleCtrl', ['$scope', '$http', function($scope, $http, apiUrl) {
+    $scope.test = apiUrl;
 }]);
