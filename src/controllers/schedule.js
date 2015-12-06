3cdbc8b176f9b6ce624636ff270ef3efc0f6790c
Adam Riddell <riddelladam42@gmail.com>
2015-12-06 23:08:51 +1100

    Remove test content from schedule controller

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -3,5 +3,4 @@
 var illuminati = angular.module('illuminati', ['illuminati-conf']);
 
 illuminati.controller('scheduleCtrl', ['$scope', '$http', function($scope, $http, apiUrl) {
-    $scope.test = apiUrl;
 }]);
