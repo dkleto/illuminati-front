fa2da5288d776603e3a56fc39445a45120b1232b
Adam Riddell <riddelladam42@gmail.com>
2016-01-07 22:52:55 +1100

    Add call to fetch schedules from API

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -3,4 +3,7 @@
 var illuminati = angular.module('illuminati', ['illuminati-conf']);
 
 illuminati.controller('scheduleCtrl', ['$scope', '$http', 'config', function($scope, $http, config) {
+    $http.get(config.apiUrl + '/schedules').success(function(data) {
+      $scope.schedules = data;
+    });
 }]);
