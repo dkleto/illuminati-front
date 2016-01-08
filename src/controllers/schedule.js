17e78471d709093f983f35c68f56647a01f4e62e
Adam Riddell <riddelladam42@gmail.com>
2016-01-08 23:59:38 +1100

    Add API error handling

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -3,7 +3,10 @@
 var illuminati = angular.module('illuminati', ['illuminati-conf']);
 
 illuminati.controller('scheduleCtrl', ['$scope', '$http', 'config', function($scope, $http, config) {
-    $http.get(config.apiUrl + '/schedules').success(function(data) {
-      $scope.schedules = data;
-    });
+    $http.get(config.apiUrl + '/schedules')
+        .success(function(data) {
+            $scope.schedules = data;
+        })
+        .error(function(data, status) {
+        });
 }]);
