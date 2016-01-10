3ab3a95fb183909d7dfb40cf575c86f216661e74
Adam Riddell <riddelladam42@gmail.com>
2016-01-10 23:08:32 +1100

    Add rough implementation of huesat to hex color

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -9,4 +9,38 @@ illuminati.controller('scheduleCtrl', ['$scope', '$http', 'config', function($sc
         })
         .error(function(data, status) {
         });
+    $scope.hexFromHsl = function(h, s, l) {
+        var r, g, b;
+
+        h = h * 1/255;
+        s = s * 1/255;
+        l = l * 1/255;
+
+        if(s == 0){
+            r = g = b = l;
+        }else{
+            var hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        var numToHex = function(number) {
+            var result = Math.floor(Math.round(number * 255) / 16).toString(16);
+            result += (Math.round(number * 255) % 16).toString(16);
+            return result;
+        }
+
+        return numToHex(r) + numToHex(g) + numToHex(b);
+    }
 }]);
