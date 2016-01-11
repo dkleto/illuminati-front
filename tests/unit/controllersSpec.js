a2ca2ce7db8fa91fcce05d97f387067947aa7c9a
Adam Riddell <riddelladam42@gmail.com>
2016-01-11 22:45:59 +1100

    Add unit test for hexFromHsl function

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -6,14 +6,18 @@ describe('Illuminati controllers', function() {
     var scope, ctrl, $httpBackend;
 
     beforeEach(module('illuminati'));
+
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
       scope = $rootScope.$new();
       $httpBackend = _$httpBackend_;
       ctrl = $controller('scheduleCtrl', {$scope: scope});
     }));
 
-    it('should include a unit test suite', function() {
-      expect(true).toBeTruthy();
+    it('should calculate the correct hex color from hsl', function() {
+      // Test a few obvious hex colour codes.
+      expect(scope.hexFromHsl(255, 255, 255)).toEqual('ffffff');
+      expect(scope.hexFromHsl(0, 0, 0)).toEqual('000000');
+      expect(scope.hexFromHsl((270/360)*255, 255, 127.5)).toEqual('7f00ff');
     });
   });
 });
