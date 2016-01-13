a4a5f47ad03c8bb14bbfdb12f61ca8b2c043b331
Adam Riddell <riddelladam42@gmail.com>
2016-01-13 23:23:38 +1100

    Add rough implementation of xy to rgb converter

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -10,7 +10,29 @@ illuminati.controller('scheduleCtrl', ['$scope', '$http', 'config', function($sc
         })
         .error(function(data, status) {
         });
+    $scope.hexFromXy = function(x, y, bri) {
+        var z = 1.0 - x - y;
+        var Y = bri;
+        var X = (Y / y) * x;
+        var Z = (Y / y) * z;
 
+        var r =  X * 1.656492 - Y * 0.354851 - Z * 0.255038;
+        var g = -X * 0.707196 + Y * 1.655397 + Z * 0.036152;
+        var b =  X * 0.051713 - Y * 0.121364 + Z * 1.011530;
+
+        r = r <= 0.0031308 ? 12.92 * r : (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
+        g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
+        b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
+
+        var numToHex = function(number) {
+            var result;
+            result = Math.floor(Math.round(number * 255) / 16).toString(16);
+            result += (Math.round(number * 255) % 16).toString(16);
+            return result;
+        }
+
+        return numToHex(r) + numToHex(g) + numToHex(b);
+    }
     $scope.hexFromHsl = function(h, s, l) {
         var r, g, b;
 
