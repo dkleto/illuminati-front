4cd80110dacac281e7899caf9081ebbc8723f719
Adam Riddell <riddelladam42@gmail.com>
2016-01-14 22:41:45 +1100

    Redesign conversion functions
        - Incorporate some new elements in the XY to rgb conversion
          from the official Hue docs.
        - Remove the incorrect HSL to rgb conversion function.

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -20,44 +20,54 @@ illuminati.controller('scheduleCtrl', ['$scope', '$http', 'config', function($sc
         var g = -X * 0.707196 + Y * 1.655397 + Z * 0.036152;
         var b =  X * 0.051713 - Y * 0.121364 + Z * 1.011530;
 
+        if (r > b && r > g && r > 1.0) {
+            // red is too big
+            g = g / r;
+            b = b / r;
+            r = 1.0;
+        }
+        else if (g > b && g > r && g > 1.0) {
+            // green is too big
+            r = r / g;
+            b = b / g;
+            g = 1.0;
+        }
+        else if (b > r && b > g && b > 1.0) {
+            // blue is too big
+            r = r / b;
+            g = g / b;
+            b = 1.0;
+        }
+
         r = r <= 0.0031308 ? 12.92 * r : (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
         g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
         b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
 
-        var numToHex = function(number) {
-            var result;
-            result = Math.floor(Math.round(number * 255) / 16).toString(16);
-            result += (Math.round(number * 255) % 16).toString(16);
-            return result;
-        }
-
-        return numToHex(r) + numToHex(g) + numToHex(b);
-    }
-    $scope.hexFromHsl = function(h, s, l) {
-        var r, g, b;
-
-        h = h / 65535;
-        s = s / 255;
-        l = l / 255;
-
-        if(s == 0){
-            r = g = b = l;
-        }else{
-            var hue2rgb = function hue2rgb(p, q, t){
-                if(t < 0) t += 1;
-                if(t > 1) t -= 1;
-                if(t < 1/6) return p + (q - p) * 6 * t;
-                if(t < 1/2) return q;
-                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-                return p;
+        if (r > b && r > g) {
+            // red is biggest
+            if (r > 1.0) {
+                g = g / r;
+                b = b / r;
+                r = 1.0;
             }
-
-            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            var p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1/3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1/3);
         }
+        else if (g > b && g > r) {
+            // green is biggest
+            if (g > 1.0) {
+                r = r / g;
+                b = b / g;
+                g = 1.0;
+            }
+        }
+        else if (b > r && b > g) {
+            // blue is biggest
+            if (b > 1.0) {
+                r = r / b;
+                g = g / b;
+                b = 1.0;
+            }
+        }
+        console.log(r+' '+g+' '+b);
 
         var numToHex = function(number) {
             var result;
