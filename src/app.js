98de62a5ba18ce892335dc28f27ac0cf24ab28a3
Adam Riddell <riddelladam42@gmail.com>
2016-02-01 18:31:52 +1100

    Add routing and move schedules list into partial

--- src/app.js
+++ src/app.js
@@ -4,3 +4,15 @@ var illuminati = angular.module('illuminati', [
     'ngRoute',
     'scheduleCtrl'
 ]);
+
+illuminati.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/schedules', {
+        templateUrl: 'partials/schedule-list.html',
+        controller: 'scheduleCtrl'
+      }).
+      otherwise({
+        redirectTo: '/schedules'
+      });
+  }]);
