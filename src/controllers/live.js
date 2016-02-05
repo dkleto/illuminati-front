a4c1ba9f8af97a7dee1a380f48844a8851d5e829
Adam Riddell <riddelladam42@gmail.com>
2016-02-05 23:58:14 +1100

    Refactor live HTTP requests slightly
        - Add request timeout.
        - Move params into config object.

--- src/controllers/live.js
+++ src/controllers/live.js
@@ -6,10 +6,17 @@ var scheduleCtrl = angular.module('liveCtrl', [
 
 scheduleCtrl.controller('liveCtrl', ['$scope', '$http', 'config', function($scope, $http, config) {
     $scope.submit = function(x, y, bri, on, trans) {
-        var data = {'x' : x, 'y' : y, 'bri' : bri, 'on' : on, 'trans' : trans};
-        $http.put(config.apiUrl + '/lights/all', data)
-            .success(function() {
-                console.log('success?');
+        var data = {x     : x,
+                    y     : y,
+                    bri   : bri,
+                    on    : on,
+                    trans : trans};
+        var putConfig = {timeout : 5000,
+                         params  : data};
+
+        $http.put(config.apiUrl + '/lights/all', '', putConfig)
+            .success(function(data) {
+                console.log('success? ' + data);
             })
             .error(function(data, status) {
                 console.log('things did not go so well... ' + data + ' ' + status);
