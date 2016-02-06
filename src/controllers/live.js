9d86a878d7691cbd089df47170007d10e054a7ec
Adam Riddell <riddelladam42@gmail.com>
2016-02-06 23:27:25 +1100

    Fix xy colour params in live route
        - Use nested xy param as expected by backend.
        - Use Jquery-like serialisation behaviour so that nested xy
          param is as expected by Grape API.

--- src/controllers/live.js
+++ src/controllers/live.js
@@ -6,20 +6,20 @@ var scheduleCtrl = angular.module('liveCtrl', [
 
 scheduleCtrl.controller('liveCtrl', ['$scope', '$http', 'config', function($scope, $http, config) {
     $scope.submit = function(x, y, bri, on, trans) {
-        var data = {x     : x,
-                    y     : y,
+        var data = {xy    : {x : x, y : y},
                     bri   : bri,
                     on    : on,
                     trans : trans};
-        var putConfig = {timeout : 5000,
-                         params  : data};
+        var putConfig = {timeout         : 5000, //TODO: Move this into config.
+                         params          : data,
+                         paramSerializer : '$httpParamSerializerJQLike'};
 
         $http.put(config.apiUrl + '/lights/all', '', putConfig)
             .success(function(data) {
-                console.log('success? ' + data);
+                console.log('success: ' + data);
             })
             .error(function(data, status) {
-                console.log('things did not go so well... ' + data + ' ' + status);
+                console.log('error: ' + data + ' ' + status);
             });
     }
     $scope.x = 0;
