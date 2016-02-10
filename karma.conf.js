430580d28508e93f449c9a6872817962824ca935
Adam Riddell <riddelladam42@gmail.com>
2016-02-10 23:12:21 +1100

    Update karma config
        - Add angular-route dependency.
        - Make sure that all JS code and tests are included.

--- karma.conf.js
+++ karma.conf.js
@@ -5,9 +5,10 @@ module.exports = function(config){
 
     files : [
       'node_modules/angular/angular.js',
+      'node_modules/angular-route/angular-route.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'dist/*.js',
-      'tests/unit/*.js'
+      'dist/**.js',
+      'tests/unit/**.js'
     ],
 
     autoWatch : true,
