ee3982745432692bf25656646bcd7d35fb4e5f23
Adam Riddell <riddelladam42@gmail.com>
2016-02-10 23:13:35 +1100

    Add unit test for colour conversion default result

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -2,16 +2,23 @@
 
 describe('Illuminati controllers', function() {
 
-  describe('scheduleCtrl', function(){
-    var scope, ctrl, $httpBackend;
+  beforeEach(module('illuminati'));
 
-    beforeEach(module('illuminati'));
+  describe('scheduleCtrl', function(){
+    var $scope, ctrl, $httpBackend;
 
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-      scope = $rootScope.$new();
       $httpBackend = _$httpBackend_;
-      ctrl = $controller('scheduleCtrl', {$scope: scope});
+      $scope = $rootScope.$new();
+      $scope.bla = 'thing';
+      ctrl = $controller('scheduleCtrl', {$scope: $scope});
     }));
 
+    it('should generate white background colour by default', function() {
+      var result = $scope.getSchedColor({});
+      expect(typeof result).toEqual('object');
+      expect(typeof result['background-color']).toEqual('string');
+      expect(result['background-color']).toEqual('#FFFFFF');
+    });
   });
 });
