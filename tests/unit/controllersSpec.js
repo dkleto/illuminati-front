f19620bfbf262bbe21138d8e0606d44faa7dd3d9
Adam Riddell <riddelladam42@gmail.com>
2016-02-18 22:43:12 +1100

    Rearrange unit test white-space obsessively

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -34,13 +34,17 @@ describe('Illuminati controllers', function() {
       expect(typeof result).toEqual('object');
       expect(result['background-color']).toEqual('#FFFFFF');
     });
+
     it('should calculate hex colour values from rgb', function() {
       // Check complete black.
       expect(scope.rgbToHex(0, 0, 0)).toEqual('#000000');
+
       // Check complete white.
       expect(scope.rgbToHex(1, 1, 1)).toEqual('#FFFFFF');
+
       // Try bright purple.
       expect(scope.rgbToHex(0.541,0.169,0.886)).toEqual('#8A2BE2')
+
       // Try bright blue.
       expect(scope.rgbToHex(0,0,1)).toEqual('#0000FF')
     });
