b1a24b79c543b275ea108dd85532011da3b6ca55
Adam Riddell <riddelladam42@gmail.com>
2016-02-25 18:10:09 +1100

    Arrange unit tests by function names

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -1,18 +1,16 @@
 'use strict';
 
-describe('Illuminati controllers', function() {
+describe('schedule controller', function() {
 
+  var scope, ctrl, $httpBackend;
   beforeEach(module('illuminati'));
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    scope = $rootScope.$new();
+    ctrl = $controller('scheduleCtrl', {$scope: scope});
+  }));
 
-  describe('scheduleCtrl', function(){
-    var scope, ctrl, $httpBackend;
-
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-      $httpBackend = _$httpBackend_;
-      scope = $rootScope.$new();
-      ctrl = $controller('scheduleCtrl', {$scope: scope});
-    }));
-
+  describe('getSchedColor', function(){
     it('should generate white background colour by default', function() {
       // Check missing xy property.
       var result = scope.getSchedColor({});
@@ -34,7 +32,8 @@ describe('Illuminati controllers', function() {
       expect(typeof result).toEqual('object');
       expect(result['background-color']).toEqual('#FFFFFF');
     });
-
+  });
+  describe('rgbToHex', function(){
     it('should calculate hex colour values from rgb', function() {
       // Check complete black.
       expect(scope.rgbToHex(0, 0, 0)).toEqual('#000000');
@@ -48,13 +47,17 @@ describe('Illuminati controllers', function() {
       // Try bright blue.
       expect(scope.rgbToHex(0,0,1)).toEqual('#0000FF')
     });
+  });
 
+  describe('xyToRgb', function(){
     it('should calculate rgb colour values from xy coordinates', function() {
       // Check bright blue.
       var blueRgb = {'r' : 0, 'g' : 0, 'b' : 1};
       expect(scope.xyToRgb(0.168, 0.041)).toEqual(blueRgb);
     });
+  });
 
+  describe('pointInGamut', function(){
     it('should check if point fits within colour gamut', function() {
       var gamutB = {
         'r' : {'x' : 0.675, 'y' : 0.322},
@@ -68,7 +71,9 @@ describe('Illuminati controllers', function() {
       // Check points that should be inside gamut.
       expect(scope.pointInGamut(0.3, 0.3, gamutB)).toEqual(true);
     });
+  });
 
+  describe('xyPoint', function(){
     it('should throw an error for invalid xy values', function() {
       expect(function() {scope.xyPoint('NaN',2);}).toThrow(new Error('"NaN" is not an integer'));
       expect(function() {scope.xyPoint(-1,2);}).toThrow(new Error('"-1" is not an integer between 0 and 1'));
