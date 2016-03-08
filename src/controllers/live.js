14f1d2346b31829e24eb0d69574860a1fad22d32
Adam Riddell <riddelladam42@gmail.com>
2016-03-09 08:28:21 +1100

    Reformat whitespace

--- src/controllers/live.js
+++ src/controllers/live.js
@@ -1,30 +1,28 @@
 'use strict';
 
-var scheduleCtrl = angular.module('liveCtrl', [
-   'illuminati-conf'
-]);
+var scheduleCtrl = angular.module('liveCtrl', ['illuminati-conf']);
 
 scheduleCtrl.controller('liveCtrl', ['$scope', '$http', 'config', function($scope, $http, config) {
-    $scope.submit = function(x, y, bri, on, trans) {
-        var data = {xy    : {x : x, y : y},
-                    bri   : bri,
-                    on    : on,
-                    transitiontime : trans};
-        var putConfig = {timeout         : 5000, //TODO: Move this into config.
-                         params          : data,
-                         paramSerializer : '$httpParamSerializerJQLike'};
+  $scope.submit = function(x, y, bri, on, trans) {
+    var data = {xy    : {x : x, y : y},
+                bri   : bri,
+                on    : on,
+                transitiontime : trans};
+    var putConfig = {timeout         : 5000, //TODO: Move this into config.
+                     params          : data,
+                     paramSerializer : '$httpParamSerializerJQLike'};
 
-        $http.put(config.apiUrl + '/lights/all', '', putConfig)
-            .success(function(data) {
-                console.log('success: ' + data);
-            })
-            .error(function(data, status) {
-                console.log('error: ' + data + ' ' + status);
-            });
-    }
-    $scope.x = 0;
-    $scope.y = 0;
-    $scope.bri = 255;
-    $scope.on = true;
-    $scope.trans = 0;
+    $http.put(config.apiUrl + '/lights/all', '', putConfig)
+      .success(function(data) {
+        console.log('success: ' + data);
+      })
+      .error(function(data, status) {
+        console.log('error: ' + data + ' ' + status);
+      });
+  };
+  $scope.x = 0;
+  $scope.y = 0;
+  $scope.bri = 255;
+  $scope.on = true;
+  $scope.trans = 0;
 }]);
