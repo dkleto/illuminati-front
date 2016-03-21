09928d451cd1c22d6ee85e5039b8a7bf1eeb7add
Adam Riddell <riddelladam42@gmail.com>
2016-03-22 07:50:23 +1100

    Prevent image dragging on live page

--- src/controllers/live.js
+++ src/controllers/live.js
@@ -2,12 +2,14 @@
 
 var scheduleCtrl = angular.module('liveCtrl', ['illuminati-conf']);
 
-scheduleCtrl.controller('liveCtrl', ['$scope', '$http', 'config', 'Color', function($scope, $http, config, Color) {
   $scope.submit = function(x, y, bri, on, trans) {
     var data = {xy    : {x : x, y : y},
                 bri   : bri,
                 on    : on,
                 transitiontime : trans};
+scheduleCtrl.controller('liveCtrl', ['$scope', '$http', 'config', 'Color', '$window', function($scope, $http, config, Color, $window) {
+  // Prevent images (e.g. color triangle) from being draggable.
+  $window.ondragstart = function() {return false;};
     var putConfig = {timeout         : 5000, //TODO: Move this into config.
                      params          : data,
                      paramSerializer : '$httpParamSerializerJQLike'};
