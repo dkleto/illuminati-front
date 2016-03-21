9908a529d4605b5aef0372ad32a12df3562d9565
Adam Riddell <riddelladam42@gmail.com>
2016-03-22 08:15:12 +1100

    Modify live page fields to do immediate update

--- src/controllers/live.js
+++ src/controllers/live.js
@@ -2,28 +2,27 @@
 
 var scheduleCtrl = angular.module('liveCtrl', ['illuminati-conf']);
 
-  $scope.submit = function(x, y, bri, on, trans) {
-    var data = {xy    : {x : x, y : y},
-                bri   : bri,
-                on    : on,
-                transitiontime : trans};
 scheduleCtrl.controller('liveCtrl', ['$scope', '$http', 'config', 'Color', '$window', function($scope, $http, config, Color, $window) {
   // Prevent images (e.g. color triangle) from being draggable.
   $window.ondragstart = function() {return false;};
+  $scope.submit = function(param, value) {
+    var data = {};
+    data[param] = value; //TODO: Add error handling for invalid params.
+    data.transitiontime = $scope.trans;
+    console.log(JSON.stringify(data));
+
     var putConfig = {timeout         : 5000, //TODO: Move this into config.
                      params          : data,
                      paramSerializer : '$httpParamSerializerJQLike'};
 
     $http.put(config.apiUrl + '/lights/all', '', putConfig)
       .success(function(data) {
-        console.log('success: ' + data);
+        console.log('success: ' + JSON.stringify(data));
       })
       .error(function(data, status) {
         console.log('error: ' + data + ' ' + status);
       });
   };
-  $scope.x = 0;
-  $scope.y = 0;
   $scope.bri = 255;
   $scope.on = true;
   $scope.trans = 0;
