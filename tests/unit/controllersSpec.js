d8feed57b5663b2a4a6b320839109e090c59f66d
Adam Riddell <riddelladam42@gmail.com>
2016-03-23 22:13:09 +1100

    Set up tests for getXy method

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -26,3 +26,40 @@ describe('schedule controller', function() {
     });
   });
 });
+
+describe('live controller', function() {
+
+  var scope, ctrl, $httpBackend;
+  beforeEach(module('illuminati'));
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    scope = $rootScope.$new();
+    ctrl = $controller('liveCtrl', {$scope: scope});
+  }));
+  describe('getXy', function() {
+    var event = {};
+    var target = {};
+    target.getBoundingClientRect = function() {
+      return this.position;
+    };
+    event.target = target;
+    event.clientX = 400;
+    event.clientY = 500;
+
+    // Height and width should be non-zero.
+    event.target.position = {left : 400,
+                             right : 500,
+                             height : 300,
+                             width : 0};
+    expect(function() {scope.getXy(event);}).toThrow(new Error('Invalid height and width values'));
+    // Height and width should be positive.
+    event.target.position = {left : 400,
+                             right : 500,
+                             height : -200,
+                             width : 300};
+    expect(function() {scope.getXy(event);}).toThrow(new Error('Invalid height and width values'));
+    // x, y should be at least equal to element left and top values.
+    // x, y should be less than or equal to width and height respectively.
+    // Result for valid input should be a valid xyPoint object.
+  });
+});
