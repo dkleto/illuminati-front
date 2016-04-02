f6d73b0d394832fcf1e60370ca2805e28f678fb0
Adam Riddell <riddelladam42@gmail.com>
2016-04-02 12:21:45 +1100

    Add angularjs-slider to karma dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,7 @@ module.exports = function(config){
 
     files : [
       'node_modules/angular/angular.js',
+      'node_modules/angularjs-slider/dist/rzslider.js',
       'node_modules/angular-route/angular-route.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'dist/**.js',
