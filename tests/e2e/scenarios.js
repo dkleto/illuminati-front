92d955a6fa87ac243eb8abbe26610d0555327ca2
Adam Riddell <riddelladam42@gmail.com>
2016-08-05 09:09:23 +1000

    Refactor schedule creation E2E test
        - Remove test code which enters values for time and transTime.
          Something about the way that sendKeys interacts with number
          input elements prevents the save schedule button from being
          clicked properly.
        - Add date string to test label.

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -12,30 +12,23 @@ describe('illuminati app', function() {
       var saveSchedule = element(by.css('.saveSchedule'));
       var scheduleList = element.all(by.repeater('schedule in schedules'));
       var scheduleLabel = element(by.model('label'));
-      var scheduleHour = element(by.model('hour'));
-      var scheduleMinute = element(by.model('minute'));
-      var scheduleTrans = element(by.model('transTime'));
 
       scheduleList.count().then(function(count) {
           addSchedule.click();
-          scheduleLabel.sendKeys('Test schedule');
-          scheduleHour.sendKeys('15');
-          scheduleMinute.sendKeys('30');
-          scheduleTrans.sendKeys('10');
+          var testLabel = 'Test ' + Date().toString();
+          scheduleLabel.sendKeys(testLabel);
           saveSchedule.click();
           expect(scheduleList.count()).toEqual(count + 1);
 
           // The final schedule in the list should be the one we just created.
           scheduleList.then(function(schedules) {
-              // Check that label, cron time and trans time were set correctly.
-              var lastSchedule = schedules[schedules.length - 1]
-              var label = lastSchedule.element(by.css('.scheduleLabel'));
-              var time = lastSchedule.element(by.css('.scheduleCronTime'));
-              var transTime = lastSchedule.element(by.css('.scheduleTransTime'));
+            // Check that label, cron time and trans time were set correctly.
+            var lastSchedule = schedules[schedules.length - 1]
+            var label = lastSchedule.element(by.css('.scheduleLabel'));
+            var time = lastSchedule.element(by.css('.scheduleCronTime'));
+            var transTime = lastSchedule.element(by.css('.scheduleTransTime'));
 
-              expect(label.getText()).toEqual('Test schedule');
-              expect(time.getText()).toEqual('Time: 15:30');
-              expect(transTime.getText()).toEqual('Transition time: 10');
+            expect(label.getText()).toEqual(testLabel);
           });
       });
     });
