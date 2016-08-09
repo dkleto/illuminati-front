bf9b0dd70f47820de57323e8bde80f2d1137b70d
Adam Riddell <riddelladam42@gmail.com>
2016-08-10 08:57:38 +1000

    Refactor schedule creation and removal E2E tests

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -12,31 +12,40 @@ describe('illuminati app', function() {
       var saveSchedule = element(by.css('.saveSchedule'));
       var scheduleList = element.all(by.repeater('schedule in schedules'));
       var scheduleLabel = element(by.model('label'));
+      var testLabel = 'Test ' + Date().toString();
 
-      // Don't proceed until we have the original schedule count.
-      scheduleList.count().then(function(count) {
-        addSchedule.click();
-        var testLabel = 'Test ' + Date().toString();
-        scheduleLabel.sendKeys(testLabel);
-        saveSchedule.click();
-        expect(scheduleList.count()).toEqual(count + 1);
+      // Run through the steps to create a new schedule.
+      protractor.promise.all([
+        scheduleList.count(),
+        addSchedule.click(),
+        scheduleLabel.sendKeys(testLabel),
+        saveSchedule.click(),
+        scheduleList.count(),
+        scheduleList
+      ]).then(function(steps) {
+        // Creating a new schedule should increment the size of the list by 1.
+        expect(steps[4]).toEqual(steps[0] + 1);
 
         // The final schedule in the list should be the one we just created.
-        scheduleList.then(function(schedules) {
-          // Check that label, cron time and trans time were set correctly.
-          var lastSchedule = schedules[schedules.length - 1]
-          var label = lastSchedule.element(by.css('.scheduleLabel'));
-          var time = lastSchedule.element(by.css('.scheduleCronTime'));
-          var transTime = lastSchedule.element(by.css('.scheduleTransTime'));
+        var schedules = steps[5];
+        var lastSchedule = schedules[schedules.length - 1]
+        var label = lastSchedule.element(by.css('.scheduleLabel'));
+        var time = lastSchedule.element(by.css('.scheduleCronTime'));
+        var transTime = lastSchedule.element(by.css('.scheduleTransTime'));
 
-          expect(label.getText()).toEqual(testLabel);
-          expect(time.getText()).toEqual('Time: 12:00');
-          expect(transTime.getText()).toEqual('Transition time: 0');
+        // Check that label, cron time and trans time were set correctly.
+        expect(label.getText()).toEqual(testLabel);
+        expect(time.getText()).toEqual('Time: 12:00');
+        expect(transTime.getText()).toEqual('Transition time: 0');
 
-          // Check that the delete button removes the schedule.
-          var deleteSchedule = lastSchedule.element(by.css('.delSchedule'));
-          deleteSchedule.click();
-          expect(scheduleList.count()).toEqual(count);
+        // Run through the steps to delete the new schedule.
+        var deleteSchedule = lastSchedule.element(by.css('.delSchedule'));
+        protractor.promise.all([
+          deleteSchedule.click(),
+          scheduleList.count()
+        ]).then(function(delSteps) {
+          // The final count of schedules should match the original count.
+          expect(delSteps[1]).toEqual(steps[0]);
         });
       });
     });
