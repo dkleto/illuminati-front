14a77b5644ba7bd1e964ea52b87970a9352e8dbf
Adam Riddell <riddelladam42@gmail.com>
2016-08-29 10:27:43 +1000

    Add hacky gulp script for offline dev
        - Add a quick and dirty script to download external
          dependencies, store them locally and replace URLs with the
          file locations. This makes it possible to develop without
          an active internet connection.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ var uglify = require('gulp-uglify')
 var concat = require('gulp-concat')
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
-var replace = require('gulp-replace-task');
+var replaceTask = require('gulp-replace-task');
 var args = require('yargs').argv;
 var fs = require('fs');
+var download = require('gulp-download');
+var rename = require('gulp-rename');
+var batchReplace = require('gulp-batch-replace');
 
 var paths = {
     files: ['src/**/*.css', 'src/**/*.html', 'src/**/*.png'],
@@ -13,6 +16,7 @@ var paths = {
     filesdist: ['dist/**/*', '!dist/**/*.js'],
     jsdist: ['dist/**/*.js', 'dist/config']
 }
+
 
 /* Pull in env variables from a json file and generate a conf.js file */
 gulp.task('replace', function () {
@@ -25,7 +29,7 @@ gulp.task('replace', function () {
   del(path + filename);
 
   gulp.src(template)
-    .pipe(replace({
+    .pipe(replaceTask({
       patterns: [
         {
           match: 'apiUrl',
@@ -77,4 +81,42 @@ gulp.task('build', ['files', 'js']);
 gulp.task('watch', function() {
     gulp.watch(paths.files, ['files']);
     gulp.watch(paths.js, ['js']);
+});
+
+// Save external dependencies for local development work.
+
+// Unfortunately dependencies are hard-coded for now...
+var deps = {'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js' : 'angular.min.js',
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.min.js' : 'angular-route.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/angularjs-slider/2.11.0/rzslider.min.js' : 'rzslider.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.3.0/angular-ui-router.min.js' : 'angular-ui-router.min.js',
+            'https://fonts.googleapis.com/css?family=Roboto:400' : 'roboto.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/angularjs-slider/2.11.0/rzslider.min.css' : 'rzslider.min.css'};
+
+gulp.task('offline', ['offline-replace'], function () {
+    download(Object.keys(deps))
+        .pipe(rename(function (path) {
+            // Get the output file name.
+            for (var url in deps) {
+                if(url.replace(/.*\//, '') === path.basename) {
+                    // Split the output filename and use it to set the path.
+                    path.extname = deps[url].replace(/.*\./, '.');
+                    var base = deps[url].match(/.*\./)[0];
+                    path.basename = base.substr(0, base.length - 1);
+                }
+            };
+        }))
+        .pipe(gulp.dest('dist/.offline'));
+});
+
+gulp.task('offline-replace', function () {
+    // Generate an array to pass to batchReplace.
+    var replaceArr = [];
+    for (var url in deps) {
+        replaceArr.push([url, '.offline/' + deps[url]]);
+    }
+
+    gulp.src('src/index.html')
+        .pipe(batchReplace(replaceArr))
+        .pipe(gulp.dest('dist'));
 });
