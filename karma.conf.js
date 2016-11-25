54bccb469ec707060d6060ddc5c7e79284150405
Adam Riddell <riddelladam42@gmail.com>
2016-11-26 06:50:52 +1100

    Remove unused dependencies from karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -5,8 +5,6 @@ module.exports = function(config){
 
     files : [
       'node_modules/angular/angular.js',
-      'node_modules/angularjs-slider/dist/rzslider.js',
-      'node_modules/angular-route/angular-route.js',
       'node_modules/angular-ui-router/release/angular-ui-router.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'node_modules/angular-material/angular-material.js',
